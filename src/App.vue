311c292fffe9295203c511b0f2880a3f15d37c90
Jared McAteer <jared.mcateer@gmail.com>
2022-05-26 12:58:53 -0600

    Add scoped slots problem

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import SlotComponent from './components/SlotComponent.vue'
 
 export default {
-  components: {HelloWorld},
+  components: {HelloWorld, SlotComponent},
 }
 </script>
 
 <template>
 <div>
   <HelloWorld :features="{foo: true}" />
+  <SlotComponent>
+    <template v-slot="props"><span>{{ props.value }}</span> </template>
+    <template #oneThing="props"><span>{{ props.value }}</span> </template>
+  </SlotComponent>
 </div>
 </template>
 
---
+++ src/components/SlotComponent.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/composition-api';
+export default defineComponent({ });
+</script>
+
+<template>
+  <div>
+    <slot name="oneThing" :value="'Hello World'"></slot>
+    <slot :value="'Hello World'"></slot>
+  </div>
+</template>
