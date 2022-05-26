1b8450a644a7d11e135e90bf4226eb9b192604a9
Jared McAteer <jared.mcateer@gmail.com>
2022-05-26 09:07:59 -0600

    init repo with example

---
+++ README.md
@@ -0,0 +1,7 @@
+# Usage
+
+`npm install`
+
+`npm run dev`
+
+Check `src/components/HelloWorld.vue` to see issue and various attempts to workaround
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,99 @@
+<script lang="ts">
+import { defineComponent, PropType } from "@vue/composition-api";
+
+export default defineComponent({
+
+  props: {
+    msg: { type: String },
+    features: { type: Object as PropType<Record<string, string | boolean>>, required: true }
+  },
+  data() {
+    return {
+      feature: 'foo',
+      count: 0
+    };
+  },
+  methods: {
+    isBoolean(feature: any): feature is boolean {
+      return typeof feature === 'boolean';
+    }
+  }
+});
+</script>
+
+<template>
+  <div>
+    <div>
+      <h1>{{ msg }}</h1>
+      <button
+        type="button"
+        @click="count++"
+      >count is: {{ count }}</button>
+
+      <!-- IDE Error -->
+      <div>
+        <h2>Error in ide</h2>
+        <label>Checkbox <input
+            type="checkbox"
+            :checked="features[feature]"
+          />
+        </label>
+        <br>
+        <img src="../assets/ide-error.png" />
+        <hr>
+        <!-- IDE Error-->
+        <h3>Also has error even with type checking</h3>
+        <input
+          type="checkbox"
+          v-if="typeof features[feature] === 'boolean'"
+          :checked="features[feature]"
+        />
+
+        <h3>Also has error checking type predicates</h3>
+        <input
+          type="checkbox"
+          v-if="isBoolean(features[feature])"
+          :checked="features[feature]"
+        />
+      </div>
+      <div>
+        <h2>Runtime error (uncomment code to see)</h2>
+        <!-- Runtime Error-->
+        <!--
+        <input
+          type="checkbox"
+          :checked="(features[feature] as boolean)"
+        />
+        -->
+
+        <br>
+        <img src="../assets/error.png" />
+
+        <h2>Works if you check property directly</h2>
+        <input
+          type="checkbox"
+          v-if="typeof features.foo === 'boolean'"
+          :checked="features.foo"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
